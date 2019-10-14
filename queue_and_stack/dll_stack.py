528dd83b8818fd5ecbdbba4ae999627accfc813f
Nabeelah <noblenab13@gmail.com>
2019-10-14 15:59:40 +0100

    Solve stack problem

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,18 +1,24 @@
-from doubly_linked_list import DoublyLinkedList
 import sys
-sys.path.append('../doubly_linked_list')
+sys.path.insert(1, '../doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
+
 
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        self.storage.add_to_head(value)
+        self.size = self.storage.__len__()
 
     def pop(self):
-        pass
+        self.size = self.storage.__len__()
+        if self.storage.head is None:
+            return
+        value = self.storage.remove_from_head()
+        return value
 
     def len(self):
-        pass
+        return self.storage.__len__()
